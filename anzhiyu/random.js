d6d66d7a52298fa2348374c62e55c3b7c78cd30a
Asparkcn <emt@emt.cc>
2024-11-07 15:25:22 +0000

    . Updated By Github Actions

---
+++ anzhiyu/random.js
@@ -0,0 +1,2 @@
+var posts=["posts/1.html","posts/2.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+//# sourceMappingURL=../maps/anzhiyu/random.js.map
---
+++ js/custom.js
@@ -0,0 +1,2 @@
+window.location.href.includes("www.aspark.cc")||window.location.href.includes("localhost")||window.location.href.includes("127.0.0.1")||window.location.replace("https://www.aspark.cc");
+//# sourceMappingURL=../maps/js/custom.js.map
