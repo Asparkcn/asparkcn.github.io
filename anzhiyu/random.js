db6ad54f9ea645233f5fe34a289e4cd14ecf1003
Asparkcn <emt@emt.cc>
2024-12-05 11:46:47 +0000

    . Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,2 +1,2 @@
-var posts=["posts/1.html","posts/2.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/3.html","posts/1.html","posts/2.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
 //# sourceMappingURL=../maps/anzhiyu/random.js.map
